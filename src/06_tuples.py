79027c737aacee179378b2e3d13a45c868fe74b4
JoshHalvorson <joshhalvorson16@gmail.com>
2019-09-03 12:48:36 -0700

    Update tuple

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -44,4 +44,5 @@ print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
 u = (1,)  # What needs to be added to make this work?
+# comma needs to be added so it knows its a tuple
 print_tuple(u)
